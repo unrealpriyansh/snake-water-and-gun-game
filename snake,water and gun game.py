0383d2e5ba403bf9a344d40ed850a635913dd57f
unrealpriyansh <86047232+unrealpriyansh@users.noreply.github.com>
2023-10-11 07:24:07 -0400

    Update snake,water and gun game.py

--- snake,water and gun game.py
+++ snake,water and gun game.py
@@ -1,45 +1,31 @@
 import random
-print("Welcome to my snake,water and gun game")
-print("Press 1 for snake")
-print("Press 2 for water")
-print("Press 3 for gun")
-
-chances=3
-l1=["Snake","Gun","Water"]
-choice_of_computer=random.choice(l1)
-for i in range(1,chances+1):
-    player_choice=int(input("Enter choice : \n"))
-    if player_choice>3:
-        print("Please give number till 3.")
-    if player_choice==1 and choice_of_computer=="Snake":
-        
-        print("Ooops,yours and computer's choice is same")
-    elif player_choice==2 and choice_of_computer=="Water":
-        
-        print("Ooops,yours and computer's choice is same")
-    elif player_choice==3 and choice_of_computer=="Gun":
-        
-        print("Ooops,yours and computer's choice is same")
-    #other matching finding.
-    elif player_choice==1 and choice_of_computer=="Water":
-        
-        print("Ooops,you drinked all the water and my choice was",choice_of_computer)
-    elif player_choice==1 and choice_of_computer=="Gun":
-        
-        print("Yeah,I killed you and my choice was",choice_of_computer)
-    elif player_choice==2 and choice_of_computer=="Snake":
-        
-        print("I drinked your all the water and my choice was",choice_of_computer)
-    elif player_choice==2 and choice_of_computer=="Gun":
-        
-        print("Oops, my gun fell in water and my choice was",choice_of_computer)
-    elif player_choice==3 and choice_of_computer=="Snake":
-        
-        print("Oops,you killed my snake and my choice was",choice_of_computer)
-    elif player_choice==3 and choice_of_computer=="Water":
-        
-        print("Yeah,your gun fell in my water and my choice was",choice_of_computer)
-if chances==3:
-    print("Game Over \n")
-    print("Thanks for playing.................")
-        
+print('''Welcome to Snake Water And Gun Game \n Press 1 for Snake.\n Press 2 for Gun.\n Press 3 for water''')
+list_of_options=["Snake","Gun","Water"]
+choice_of_computer=random.choice(list_of_options)
+for poplu in range(3):
+    choice_of_computer=random.choice(list_of_options)
+    user_choice=int(input("Enter a number between 1 and 3 : \n"))
+    if (user_choice==1):
+        if (choice_of_computer=="Snake"):
+            print("Ohh!! My choice was also same.Our snake started fighting.")
+        elif(choice_of_computer=="Gun"):
+            print("I killed your snake.My choice was gun")
+        elif(choice_of_computer=="Water"):
+            print("My choice was water and your snake drank all the water")
+    if(user_choice==2):
+        if(choice_of_computer=="Snake"):
+            print("You killed my snake.My choice was snake")
+        elif(choice_of_computer=="Gun"):
+            print("A big collision of our guns...!! My choice was gun.")
+        elif(choice_of_computer=="Water"):
+            print("Your gun fell in the water. My choice was water")
+    if (user_choice==3):
+        if(choice_of_computer=="Snake"):
+            print("My snake drank all the water.My choice was snake")
+        elif(choice_of_computer=="Gun"):
+            print("My gun fell in the water.My choice was gun.")
+        elif(choice_of_computer=="Water"):
+            print("Ohhh!! Same choices..Lot  of water there..!!!")
+    if(user_choice>=4):
+        print("Please give number between 1 and 3")
+print('''Game Over\nThanks for playing.....''')
